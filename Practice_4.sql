60add7738158a30025a895754e0022911dd266fb
M'Lee Law (muh-LEE) <mleelaw123@gmail.com>
2025-04-14 21:24:28 -0500

    Delete Practice_4.sql

--- Practice_4.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    e.first_name || ' ' || e.last_name AS full_name,
-    d.business_name
-FROM
-    employees e
-INNER JOIN dealershipemployees de ON e.employee_id = de.employee_id
-INNER JOIN dealerships d ON de.dealership_id = d.dealership_id
